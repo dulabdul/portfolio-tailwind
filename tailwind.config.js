6bbe4246b373af1c45a1c6d623f2176a7e6ccb32
dulabdul <dul.abdoel21@gmail.com>
2022-11-19 08:59:34 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        main: '#03ad7a',
+        mainHover: '#02996c',
+      },
+    },
+  },
+  plugins: [],
+};
